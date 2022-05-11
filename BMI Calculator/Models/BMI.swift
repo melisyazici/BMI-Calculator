5d8f718e9a2fe98b7e5d1800f3ba29c6673f2f2a
Melis Yazıcı <melis.yazici95@icloud.com>
2022-05-12 00:17:53 +0200

    first commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,14 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Melis Yazıcı on 05.05.22.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
